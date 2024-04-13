40b2abce0333e4a796a92e16c18f5f159b71e114
Roy <rrtc26@gmail.com>
2024-04-12 23:29:05 -0500

    Conectionblis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
@@ -45,4 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation (libs.okhttp)
 }
--- app/src/main/java/com/marmary/saludarte/MainActivity.kt
+++ app/src/main/java/com/marmary/saludarte/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.marmary.saludarte
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.websocketdeneme.PieSocketListener
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button = findViewById<Button>(R.id.button)
+        val client: OkHttpClient =  OkHttpClient()
+
+        button.setOnClickListener {
+            Log.d("PieSocket","Connecting");
+
+            val request: Request = Request
+                .Builder()
+                .url("http://172.20.10.3:7889")
+                .build()
+            val listener = PieSocketListener()
+            val ws: WebSocket = client.newWebSocket(request, listener)
+        }
+
+
+
+
     }
+
+
+
+
+
+
 }
